bcf827e0e0e35aa4bb5df8e27b36679e5e9b55c6
unknown <israelcastejon1@gmail.com>
2022-09-27 18:54:50 +0200

    añado src

---
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejercicioselementales.java
@@ -0,0 +1,10 @@
+
+package com.tiernoparla.ejercicioselementales;
+
+
+public class ejercicioselementales {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }//main
+            
+}//ejercicioselementales
