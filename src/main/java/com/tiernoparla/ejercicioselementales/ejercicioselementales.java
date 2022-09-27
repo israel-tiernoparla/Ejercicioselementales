38026a1d350fe1bac23eca559a9cda08477b1884
unknown <israelcastejon1@gmail.com>
2022-09-27 20:10:50 +0200

    ejercicio print pares

--- src/main/java/com/tiernoparla/ejercicioselementales/ejercicioselementales.java
+++ src/main/java/com/tiernoparla/ejercicioselementales/ejercicioselementales.java
@@ -4,7 +4,16 @@ package com.tiernoparla.ejercicioselementales;
 
 public class ejercicioselementales {
     public static void main(String[] args) {
-        System.out.println("hola");
+        
+        // printear numeros pares del 1 al 10
+       for(int i = 2; i < 10; i = i+2){  
+           System.out.println(i);  
+       
+       }//for
+   
+    
+    
+    
     }//main
             
 }//ejercicioselementales
